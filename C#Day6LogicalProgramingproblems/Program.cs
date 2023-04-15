f8aa3bce5475cac7272e034e11c023566c412097
Sushil <skshinde2028@gmail.com>
2023-04-15 12:54:09 +0530

    [Sushil] adding Welcome message

---
+++ C#Day6LogicalProgramingproblems/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Day6LogicalProgramingproblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the c# basic logical programs");
+        }
+    }
+}

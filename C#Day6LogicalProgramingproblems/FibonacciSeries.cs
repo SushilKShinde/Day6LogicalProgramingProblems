d5ba49474867b1dbf175fa5b40e9554889164d5a
Sushil <skshinde2028@gmail.com>
2023-04-15 14:16:09 +0530

    [Sushil] adding Fibonacci series code

---
+++ C#Day6LogicalProgramingproblems/FibonacciSeries.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Day6LogicalProgramingproblems
+{
+    internal class FibonacciSeries
+    {
+        // In Fibonacci Series next number is sum of previous two numbers
+        public void FindFibonacciSerries(int num)
+        {   
+            int n1 = 0;
+            int n2 = 1;
+            int n3;
+            Console.Write("The fibonacci series is: {0} {1} ", n1, n2);
+
+            for (int i = 0; i <=num; i++)
+            {
+                n3 = n1 + n2; 
+                Console.Write(n3 + " ");    
+                n1 = n2; 
+                n2 = n3; 
+            }
+        }
+    }
+}
--- C#Day6LogicalProgramingproblems/Program.cs
+++ C#Day6LogicalProgramingproblems/Program.cs
@@ -11,6 +11,22 @@ namespace C_Day6LogicalProgramingproblems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the c# basic logical programs");
+            Console.WriteLine("Choose the program option from below options");
+            Console.WriteLine("1.Fibonacci Series");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                    Console.WriteLine("Enter the range of series");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    fibonacciSeries.FindFibonacciSerries(num);
+                    break;
+                default:
+                    Console.WriteLine("Please enter valid option.");
+                    break;
+            }
         }
     }
 }

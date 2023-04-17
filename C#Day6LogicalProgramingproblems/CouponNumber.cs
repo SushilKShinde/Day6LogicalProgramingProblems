045dcbdf2d6c908b1a01e57b870d1b6d91ff96de
Sushil <skshinde2028@gmail.com>
2023-04-17 15:40:50 +0530

    [Sushil] coupon number

---
+++ C#Day6LogicalProgramingproblems/CouponNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Day6LogicalProgramingproblems
+{
+    internal class CouponNumber
+    {
+        public static void FindCouponNumbers()
+        {
+            Console.WriteLine("Enter the range ");
+            int range = Convert.ToInt32(Console.ReadLine());
+            int[] coupons = new int[range];
+            Random random = new Random();
+            //int n = 10;
+            int index = 0;
+            for (int i=0; i<range; i++)
+            { 
+                int number = random.Next(1, 11);
+                if (coupons.Contains(number)==false)
+                {
+                    coupons[index] = number;
+                    index++;
+                }
+            }
+            //printing coupon numbers
+            Console.WriteLine("The coupon numbers are: ");
+            for (int i=0; i<=coupons.Length;i++)
+            {
+                Console.Write(" "+coupons[i]);
+            }
+        }
+    }
+}

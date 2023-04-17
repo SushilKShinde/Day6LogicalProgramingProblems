a5e0663689917778b283d26ce3d980a1f5ae60c3
Sushil <skshinde2028@gmail.com>
2023-04-17 21:12:46 +0530

    [Sushil] Finding day on the date

---
+++ C#Day6LogicalProgramingproblems/DayOfWeek.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Day6LogicalProgramingproblems
+{
+    internal class DayOfWeek
+    {
+        //int y0;
+        public static void FindDayOfweek(int d, int m, int y)
+        {
+            int  y0 = y - (14 - m) / 12;
+            int x = y0 + y0 / 12 - y0 / 100 + y0 / 400;
+            int m0 = m + 12 * ((14 - m) / 12) - 2; 
+            int d0 = (d + x + (31 * m0) / 12) % 7;
+
+            switch(m)
+            {
+                case 1:
+                    Console.WriteLine("January");
+                    break;
+                case 2:
+                    Console.WriteLine("Frebruary");
+                    break;
+                case 3:
+                    Console.WriteLine("March");
+                    break;
+                case 4:
+                    Console.WriteLine("April");
+                    break;
+                case 5:
+                    Console.WriteLine("May");
+                    break;
+                case 6:
+                    Console.WriteLine("June");
+                    break;
+                case 7:
+                    Console.WriteLine("July");
+                    break;
+                case 8:
+                    Console.WriteLine("Agust");
+                    break;
+                case 9:
+                    Console.WriteLine("September");
+                    break;
+                case 10:
+                    Console.WriteLine("October");
+                    break;
+                case 11:
+                    Console.WriteLine("November");
+                    break;
+                case 12:
+                    Console.WriteLine("December");
+                    break;
+
+            }
+
+            switch (d0)
+            {
+                case 0:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednsday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+            }
+        }
+    }
+}
--- C#Day6LogicalProgramingproblems/Program.cs
+++ C#Day6LogicalProgramingproblems/Program.cs
@@ -13,7 +13,7 @@ namespace C_Day6LogicalProgramingproblems
             Console.WriteLine("Welcome to the c# basic logical programs");
             Console.WriteLine("Choose the program option from below options");
             Console.WriteLine("1.Fibonacci Series\n2.PrimeNumber\n3.ReverseANumber\n4.CouponNumber\n5.Stopwatch" +
-                "\n6.PerfectNumber");
+                "\n6.PerfectNumber\n7.DayOfWeek");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -45,6 +45,16 @@ namespace C_Day6LogicalProgramingproblems
                     Console.WriteLine("Enter a integer number");
                     int num3 = Convert.ToInt32(Console.ReadLine());
                     PerfectNumber.CheckForPerfectNumber(num3);
+                    break;
+                case 7:
+                    Console.WriteLine("Please enter the date in format DD/MM/YYYY");
+                    Console.WriteLine("Enter a date");
+                    int day = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter a month");
+                    int month = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter a yeae");
+                    int year = Convert.ToInt32(Console.ReadLine());
+                    DayOfWeek.FindDayOfweek(day, month, year);
                     break;
                 default:
                     Console.WriteLine("Please enter valid option.");

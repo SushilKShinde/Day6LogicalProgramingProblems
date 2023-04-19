ce8e34d172cffc1f3afc9c4d17cb3ea05eb93d13
Sushil <skshinde2028@gmail.com>
2023-04-19 12:41:02 +0530

    [Sushil] Converting decimal number into binary

---
+++ C#Day6LogicalProgramingproblems/BinaryToDecimal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Day6LogicalProgramingproblems
+{
+    internal class DecimalToBinary
+    {
+        public static void ConvertNumberToBinary(int num)
+        {
+            string binary = " ";           
+            int n;
+
+            while (num>0)
+            {
+                n = num % 2;
+                binary = Convert.ToString(n) + binary;
+                num = num / 2;
+            }
+            Console.WriteLine("Binary Conversion: "+ binary);
+        }
+    } 
+}
